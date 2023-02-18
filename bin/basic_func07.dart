5efe1600231a16ea13f4b6375deb33ff2248d73d
network7277 <husanjurayev7277@gmail.com>
2023-02-18 16:27:37 +0500

    07

--- bin/basic_func07.dart
+++ bin/basic_func07.dart
@@ -3,4 +3,10 @@
 //  Create function arguments a.
 
 //  Return the value a.
-void main() {}
+  String func(String a){
+    return 'my name is ' + a;
+}
+    
+void main() {
+    print(func(' Husan'));
+}
